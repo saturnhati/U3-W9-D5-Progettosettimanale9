4deb870912ae4502a81786cfb89e5111883d38d4
saturnhati <76071008+saturnhati@users.noreply.github.com>
2022-10-30 17:00:48 +0100

    fixed loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 "use strict";
 class Smartphone {
-    constructor() {
+    constructor(tipo) {
         this.saldo = 0;
         this.numero_chiamate = 0;
+        this.tipo = tipo;
     }
     ricarica(importo) { }
     chiamata(minuti) { }
@@ -67,13 +68,13 @@ class Utente {
     }
 }
 let user1 = new Utente("Mario", "Rossi");
-let iphone = new Iphone();
+let iphone = new Iphone("IPhone");
 user1.smartphone = iphone;
 let user2 = new Utente("Luigi", "Verdi");
-let galaxy = new SamsungGalaxy();
+let galaxy = new SamsungGalaxy("Samsung");
 user2.smartphone = galaxy;
 let user3 = new Utente("Luca", "Rota");
-let huawei = new Huawei();
+let huawei = new Huawei("Huawei");
 user3.smartphone = huawei;
 // *prove in console
 // // mostro il saldo del telefono
@@ -94,125 +95,125 @@ user3.smartphone = huawei;
 // user1.smartphone.getNumeroChiamate();
 // console.log(user1.smartphone);
 // *fine prove in console
+let userArr = [user1, user2, user3];
+let bgArr = ["url('assets/img/iphone.png')", "url('assets/img/galaxy.png')", "url('assets/img/huawei.png')"];
 function displayContent() {
-    let iphoneScreen = document.querySelector(".iphone-screen");
-    let galaxyScreen = document.querySelector(".galaxy-screen");
-    let huaweiScreen = document.querySelector(".huawei-screen");
-    // !IPHONE
-    //   creo il div infos
-    let divInfos = document.createElement("div");
-    divInfos === null || divInfos === void 0 ? void 0 : divInfos.classList.add("infos");
-    // creo h3 e ci scrivo
-    let h3Name = document.createElement("h3");
-    h3Name.innerHTML = `iPhone di ${user1.name} ${user1.surname}`;
-    // creo h3 e ci scrivo
-    let h2Title = document.createElement("h2");
-    h2Title.innerHTML = "Ricarica:";
-    // creo il bottone 5€ metto l'id e creo l'event listener
-    let ricButton5 = document.createElement("button");
-    ricButton5.setAttribute("id", "ric5");
-    ricButton5.innerHTML = "5€";
-    ricButton5.addEventListener("click", () => {
-        var _a, _b;
-        (_a = user1.smartphone) === null || _a === void 0 ? void 0 : _a.ricarica(5);
-        divConsole.innerHTML = `La ricarica di 5€ è stata effettuata. Nuovo credito: ${(_b = user1.smartphone) === null || _b === void 0 ? void 0 : _b.saldo}€`;
-    });
-    // creo il bottone 10€ metto l'id e creo l'event listener
-    let ricButton10 = document.createElement("button");
-    ricButton10.innerHTML = "10€";
-    ricButton10.setAttribute("id", "ric10");
-    ricButton10.addEventListener("click", () => {
-        var _a, _b;
-        (_a = user1.smartphone) === null || _a === void 0 ? void 0 : _a.ricarica(10);
-        divConsole.innerHTML = `La ricarica di 10€ è stata effettuata. Nuovo credito: ${(_b = user1.smartphone) === null || _b === void 0 ? void 0 : _b.saldo}€`;
-    });
-    // creo il bottone 15€ metto l'id e creo l'event listener
-    let ricButton15 = document.createElement("button");
-    ricButton15.innerHTML = "15€";
-    ricButton15.setAttribute("id", "ric15");
-    ricButton15.addEventListener("click", () => {
-        var _a, _b;
-        (_a = user1.smartphone) === null || _a === void 0 ? void 0 : _a.ricarica(15);
-        divConsole.innerHTML = `La ricarica di 15€ è stata effettuata. Nuovo credito: ${(_b = user1.smartphone) === null || _b === void 0 ? void 0 : _b.saldo}€`;
-    });
-    //   creo il bottone per visualizzare il saldo
-    let btnSaldo = document.createElement("button");
-    btnSaldo.classList.add("btn-big");
-    btnSaldo.innerHTML = "SALDO";
-    btnSaldo.addEventListener("click", () => {
-        var _a, _b;
-        (_a = user1.smartphone) === null || _a === void 0 ? void 0 : _a.numero404();
-        divConsole.innerHTML = `Il tuo saldo è: ${(_b = user1.smartphone) === null || _b === void 0 ? void 0 : _b.saldo}€`;
-    });
-    //   creo il bottone per visualizzare il numero di chiamate
-    let btnChiamate = document.createElement("button");
-    btnChiamate.innerHTML = "chiamate";
-    btnChiamate.classList.add("btn-big");
-    btnChiamate.addEventListener("click", () => {
-        var _a;
-        divConsole.innerHTML = `Chiamate effettuate: ${(_a = user1.smartphone) === null || _a === void 0 ? void 0 : _a.getNumeroChiamate()}`;
-    });
-    //   creo il bottone per azzerare le chiamate
-    let btnAzzera = document.createElement("button");
-    btnAzzera.innerHTML = "azzera chiamate";
-    btnAzzera.classList.add("btn-big");
-    btnAzzera.addEventListener("click", () => {
-        var _a;
-        (_a = user1.smartphone) === null || _a === void 0 ? void 0 : _a.azzeraChiamate();
-        divConsole.innerHTML = `Chiamate effettuate: 0`;
-    });
-    // inserisco tutto nel div infos
-    divInfos.append(h3Name, h2Title, ricButton5, ricButton10, ricButton15, btnSaldo, btnChiamate, btnAzzera);
-    //   inserisco il div infos nel div totale screen
-    iphoneScreen.appendChild(divInfos);
-    //   creo il div Console e lo aggiungo al div totale screen
-    let divConsole = document.createElement("div");
-    divConsole.classList.add("iphone-console");
-    iphoneScreen.appendChild(divConsole);
-    // creo il bottone chiamata e lo aggiungo al div totale screen
-    let callBtn = document.createElement("button");
-    callBtn.setAttribute("id", "btn-iphone");
-    callBtn.innerHTML = '<i class="fa-solid fa-phone"></i>';
-    let onCall = false;
-    callBtn.addEventListener("click", () => {
-        var _a, _b;
-        if (!onCall) {
-            startTimer(divConsole);
-            onCall = true;
-            callBtn.style.backgroundColor = "red";
-        }
-        else {
-            onCall = false;
-            callBtn.style.backgroundColor = "";
-            let minuti = (interval * 2) / 10 + 1;
-            clearInterval(interval);
-            console.log(minuti);
-            (_a = user1.smartphone) === null || _a === void 0 ? void 0 : _a.chiamata(minuti);
-            divConsole.innerHTML = `Chiamata terminata. Costo totale: ${(_b = user1.smartphone) === null || _b === void 0 ? void 0 : _b.chiamata(minuti)}€`;
-        }
-    });
-    iphoneScreen.appendChild(callBtn);
-    // !GALAXY
-    galaxyScreen.innerHTML = `
-        <div class="infos">
-            <h3>Samsung di ${user2.name} ${user2.surname}</h3>
-        </div>
-        <div class="galaxy-console"></div>
-        <button id="btn-galaxy"><i class="fa-solid fa-phone"></i></button>
-        `;
-    // !HUAWEI
-    huaweiScreen.innerHTML = `
-        <div class="infos">
-            <h3>Huawei di ${user3.name} ${user3.surname}</h3>
-        </div>
-        <div class="huawei-console"></div>
-        <button id="btn-huawei"><i class="fa-solid fa-phone"></i></button>
-        `;
+    var _a;
+    let container = document.querySelector(".container");
+    // !SMARTPHONE DISPLAY
+    let j = 0;
+    for (let user of userArr) {
+        //   creo il div PHONE
+        let divContainer = document.createElement("div");
+        divContainer.classList.add("phone");
+        divContainer.style.backgroundImage = bgArr[j];
+        j++;
+        // creo il div PHONESCREEN
+        let phoneScreen = document.createElement("div");
+        phoneScreen.classList.add("phone-screen");
+        //   creo il div infos
+        let divInfos = document.createElement("div");
+        divInfos === null || divInfos === void 0 ? void 0 : divInfos.classList.add("infos");
+        // creo h3 e ci scrivo
+        let h3Name = document.createElement("h3");
+        h3Name.innerHTML = `${(_a = user.smartphone) === null || _a === void 0 ? void 0 : _a.tipo} di ${user.name} ${user.surname}`;
+        // creo h3 e ci scrivo
+        let h2Title = document.createElement("h2");
+        h2Title.innerHTML = "Ricarica:";
+        // creo il bottone 5€ metto l'id e creo l'event listener
+        let ricButton5 = document.createElement("button");
+        ricButton5.setAttribute("id", "ric5");
+        ricButton5.innerHTML = "5€";
+        ricButton5.addEventListener("click", () => {
+            var _a, _b;
+            (_a = user.smartphone) === null || _a === void 0 ? void 0 : _a.ricarica(5);
+            divConsole.innerHTML = `La ricarica di 5€ è stata effettuata. Nuovo credito: ${(_b = user.smartphone) === null || _b === void 0 ? void 0 : _b.saldo}€`;
+        });
+        // creo il bottone 10€ metto l'id e creo l'event listener
+        let ricButton10 = document.createElement("button");
+        ricButton10.innerHTML = "10€";
+        ricButton10.setAttribute("id", "ric10");
+        ricButton10.addEventListener("click", () => {
+            var _a, _b;
+            (_a = user.smartphone) === null || _a === void 0 ? void 0 : _a.ricarica(10);
+            divConsole.innerHTML = `La ricarica di 10€ è stata effettuata. Nuovo credito: ${(_b = user.smartphone) === null || _b === void 0 ? void 0 : _b.saldo}€`;
+        });
+        // creo il bottone 15€ metto l'id e creo l'event listener
+        let ricButton15 = document.createElement("button");
+        ricButton15.innerHTML = "15€";
+        ricButton15.setAttribute("id", "ric15");
+        ricButton15.addEventListener("click", () => {
+            var _a, _b;
+            (_a = user.smartphone) === null || _a === void 0 ? void 0 : _a.ricarica(15);
+            divConsole.innerHTML = `La ricarica di 15€ è stata effettuata. Nuovo credito: ${(_b = user.smartphone) === null || _b === void 0 ? void 0 : _b.saldo}€`;
+        });
+        //   creo il bottone per visualizzare il saldo
+        let btnSaldo = document.createElement("button");
+        btnSaldo.classList.add("btn-big");
+        btnSaldo.innerHTML = "SALDO";
+        btnSaldo.addEventListener("click", () => {
+            var _a, _b;
+            (_a = user.smartphone) === null || _a === void 0 ? void 0 : _a.numero404();
+            divConsole.innerHTML = `Il tuo saldo è: ${(_b = user.smartphone) === null || _b === void 0 ? void 0 : _b.saldo}€`;
+        });
+        //   creo il bottone per visualizzare il numero di chiamate
+        let btnChiamate = document.createElement("button");
+        btnChiamate.innerHTML = "chiamate";
+        btnChiamate.classList.add("btn-big");
+        btnChiamate.addEventListener("click", () => {
+            var _a;
+            divConsole.innerHTML = `Chiamate effettuate: ${(_a = user.smartphone) === null || _a === void 0 ? void 0 : _a.getNumeroChiamate()}`;
+        });
+        //   creo il bottone per azzerare le chiamate
+        let btnAzzera = document.createElement("button");
+        btnAzzera.innerHTML = "azzera chiamate";
+        btnAzzera.classList.add("btn-big");
+        btnAzzera.addEventListener("click", () => {
+            var _a;
+            (_a = user.smartphone) === null || _a === void 0 ? void 0 : _a.azzeraChiamate();
+            divConsole.innerHTML = `Chiamate effettuate: 0`;
+        });
+        // inserisco tutto nel div infos
+        divInfos.append(h3Name, h2Title, ricButton5, ricButton10, ricButton15, btnSaldo, btnChiamate, btnAzzera);
+        //   inserisco il div infos nel div totale screen
+        phoneScreen.appendChild(divInfos);
+        //   creo il div Console e lo aggiungo al div totale screen
+        let divConsole = document.createElement("div");
+        divConsole.classList.add("phone-console");
+        phoneScreen.appendChild(divConsole);
+        // creo il bottone chiamata e lo aggiungo al div totale screen
+        let callBtn = document.createElement("button");
+        callBtn.setAttribute("id", "btn-phone");
+        callBtn.innerHTML = '<i class="fa-solid fa-phone"></i>';
+        let onCall = false;
+        callBtn.addEventListener("click", () => {
+            var _a, _b;
+            if (!onCall) {
+                startTimer(divConsole);
+                onCall = true;
+                callBtn.style.backgroundColor = "red";
+            }
+            else {
+                onCall = false;
+                callBtn.style.backgroundColor = "";
+                clearInterval(interval);
+                console.log(Number(s));
+                let minuti = s;
+                console.log(minuti);
+                (_a = user.smartphone) === null || _a === void 0 ? void 0 : _a.chiamata(minuti);
+                divConsole.innerHTML = `Chiamata terminata. Costo totale: ${(_b = user.smartphone) === null || _b === void 0 ? void 0 : _b.chiamata(minuti)}€`;
+            }
+        });
+        phoneScreen.appendChild(callBtn);
+        divContainer.appendChild(phoneScreen);
+        container.appendChild(divContainer);
+    }
 }
 onload = () => {
     displayContent();
 };
 let interval;
+let s;
 function startTimer(elem) {
     let s = 1;
     interval = setInterval(function () {
@@ -224,6 +225,7 @@ function startTimer(elem) {
         }
         s++;
     }, 1000);
+    console.log(s);
+    return s;
 }
 // TODO: sistemare funzionamento del timer delle chiamate che mo pare drogato
-// TODO: altri due smartphone - (forse un ciclo? array di user 1 - 2 - 3 ecc ????)
--- assets/ts/script.ts
+++ assets/ts/script.ts
@@ -1,6 +1,7 @@
 interface ISmartphone {
   saldo: number;
   numero_chiamate: number;
+  tipo: string;
 
   ricarica(importo: number): void;
   chiamata(minuti: number): void;
@@ -12,9 +13,11 @@ interface ISmartphone {
 abstract class Smartphone implements ISmartphone {
   saldo: number;
   numero_chiamate: number;
-  constructor() {
+  tipo: string;
+  constructor(tipo: string) {
     this.saldo = 0;
     this.numero_chiamate = 0;
+    this.tipo = tipo;
   }
 
   ricarica(importo: number): any {}
@@ -88,15 +91,15 @@ class Utente {
 }
 
 let user1 = new Utente("Mario", "Rossi");
-let iphone = new Iphone();
+let iphone = new Iphone("IPhone");
 user1.smartphone = iphone;
 
 let user2 = new Utente("Luigi", "Verdi");
-let galaxy = new SamsungGalaxy();
+let galaxy = new SamsungGalaxy("Samsung");
 user2.smartphone = galaxy;
 
 let user3 = new Utente("Luca", "Rota");
-let huawei = new Huawei();
+let huawei = new Huawei("Huawei");
 user3.smartphone = huawei;
 
 // *prove in console
@@ -119,114 +122,112 @@ user3.smartphone = huawei;
 // console.log(user1.smartphone);
 // *fine prove in console
 
+let userArr: Utente[] = [user1, user2, user3];
+let bgArr: string[] = ["url('assets/img/iphone.png')", "url('assets/img/galaxy.png')", "url('assets/img/huawei.png')"];
+
 function displayContent() {
-  let iphoneScreen = document.querySelector(".iphone-screen") as HTMLElement;
-  let galaxyScreen = document.querySelector(".galaxy-screen") as HTMLElement;
-  let huaweiScreen = document.querySelector(".huawei-screen") as HTMLElement;
-  // !IPHONE
-  //   creo il div infos
-  let divInfos = document.createElement("div");
-  divInfos?.classList.add("infos");
-  // creo h3 e ci scrivo
-  let h3Name = document.createElement("h3");
-  h3Name.innerHTML = `iPhone di ${user1.name} ${user1.surname}`;
-  // creo h3 e ci scrivo
-  let h2Title = document.createElement("h2");
-  h2Title.innerHTML = "Ricarica:";
-  // creo il bottone 5€ metto l'id e creo l'event listener
-  let ricButton5 = document.createElement("button");
-  ricButton5.setAttribute("id", "ric5");
-  ricButton5.innerHTML = "5€";
-  ricButton5.addEventListener("click", () => {
-    user1.smartphone?.ricarica(5);
-    divConsole.innerHTML = `La ricarica di 5€ è stata effettuata. Nuovo credito: ${user1.smartphone?.saldo}€`;
-  });
-  // creo il bottone 10€ metto l'id e creo l'event listener
-  let ricButton10 = document.createElement("button");
-  ricButton10.innerHTML = "10€";
-  ricButton10.setAttribute("id", "ric10");
-  ricButton10.addEventListener("click", () => {
-    user1.smartphone?.ricarica(10);
-    divConsole.innerHTML = `La ricarica di 10€ è stata effettuata. Nuovo credito: ${user1.smartphone?.saldo}€`;
-  });
-  // creo il bottone 15€ metto l'id e creo l'event listener
-  let ricButton15 = document.createElement("button");
-  ricButton15.innerHTML = "15€";
-  ricButton15.setAttribute("id", "ric15");
-  ricButton15.addEventListener("click", () => {
-    user1.smartphone?.ricarica(15);
-    divConsole.innerHTML = `La ricarica di 15€ è stata effettuata. Nuovo credito: ${user1.smartphone?.saldo}€`;
-  });
-  //   creo il bottone per visualizzare il saldo
-  let btnSaldo = document.createElement("button");
-  btnSaldo.classList.add("btn-big");
-  btnSaldo.innerHTML = "SALDO";
-  btnSaldo.addEventListener("click", () => {
-    user1.smartphone?.numero404();
-    divConsole.innerHTML = `Il tuo saldo è: ${user1.smartphone?.saldo}€`;
-  });
-  //   creo il bottone per visualizzare il numero di chiamate
-  let btnChiamate = document.createElement("button");
-  btnChiamate.innerHTML = "chiamate";
-  btnChiamate.classList.add("btn-big");
-  btnChiamate.addEventListener("click", () => {
-    divConsole.innerHTML = `Chiamate effettuate: ${user1.smartphone?.getNumeroChiamate()}`;
-  });
-  //   creo il bottone per azzerare le chiamate
-  let btnAzzera = document.createElement("button");
-  btnAzzera.innerHTML = "azzera chiamate";
-  btnAzzera.classList.add("btn-big");
-  btnAzzera.addEventListener("click", () => {
-    user1.smartphone?.azzeraChiamate();
-    divConsole.innerHTML = `Chiamate effettuate: 0`;
-  });
-  // inserisco tutto nel div infos
-  divInfos.append(h3Name, h2Title, ricButton5, ricButton10, ricButton15, btnSaldo, btnChiamate, btnAzzera);
-  //   inserisco il div infos nel div totale screen
-  iphoneScreen.appendChild(divInfos);
-  //   creo il div Console e lo aggiungo al div totale screen
-  let divConsole = document.createElement("div");
-  divConsole.classList.add("iphone-console");
-  iphoneScreen.appendChild(divConsole);
-  // creo il bottone chiamata e lo aggiungo al div totale screen
-  let callBtn = document.createElement("button");
-  callBtn.setAttribute("id", "btn-iphone");
-  callBtn.innerHTML = '<i class="fa-solid fa-phone"></i>';
-  let onCall: boolean = false;
-  callBtn.addEventListener("click", () => {
-    if (!onCall) {
-      startTimer(divConsole);
-      onCall = true;
-      callBtn.style.backgroundColor = "red";
-    } else {
-      onCall = false;
-      callBtn.style.backgroundColor = "";
-      let minuti: number = (interval * 2) / 10 + 1;
-      clearInterval(interval);
-      console.log(minuti);
-      user1.smartphone?.chiamata(minuti);
-      divConsole.innerHTML = `Chiamata terminata. Costo totale: ${user1.smartphone?.chiamata(minuti)}€`;
-    }
-  });
-  iphoneScreen.appendChild(callBtn);
+  let container = document.querySelector(".container") as HTMLElement;
 
-  // !GALAXY
-  galaxyScreen.innerHTML = `
-        <div class="infos">
-            <h3>Samsung di ${user2.name} ${user2.surname}</h3>
-        </div>
-        <div class="galaxy-console"></div>
-        <button id="btn-galaxy"><i class="fa-solid fa-phone"></i></button>
-        `;
-
-  // !HUAWEI
-  huaweiScreen.innerHTML = `
-        <div class="infos">
-            <h3>Huawei di ${user3.name} ${user3.surname}</h3>
-        </div>
-        <div class="huawei-console"></div>
-        <button id="btn-huawei"><i class="fa-solid fa-phone"></i></button>
-        `;
+  // !SMARTPHONE DISPLAY
+  let j = 0;
+  for (let user of userArr) {
+    //   creo il div PHONE
+    let divContainer = document.createElement("div");
+    divContainer.classList.add("phone");
+    divContainer.style.backgroundImage = bgArr[j];
+    j++;
+    // creo il div PHONESCREEN
+    let phoneScreen = document.createElement("div");
+    phoneScreen.classList.add("phone-screen");
+    //   creo il div infos
+    let divInfos = document.createElement("div");
+    divInfos?.classList.add("infos");
+    // creo h3 e ci scrivo
+    let h3Name = document.createElement("h3");
+    h3Name.innerHTML = `${user.smartphone?.tipo} di ${user.name} ${user.surname}`;
+    // creo h3 e ci scrivo
+    let h2Title = document.createElement("h2");
+    h2Title.innerHTML = "Ricarica:";
+    // creo il bottone 5€ metto l'id e creo l'event listener
+    let ricButton5 = document.createElement("button");
+    ricButton5.setAttribute("id", "ric5");
+    ricButton5.innerHTML = "5€";
+    ricButton5.addEventListener("click", () => {
+      user.smartphone?.ricarica(5);
+      divConsole.innerHTML = `La ricarica di 5€ è stata effettuata. Nuovo credito: ${user.smartphone?.saldo}€`;
+    });
+    // creo il bottone 10€ metto l'id e creo l'event listener
+    let ricButton10 = document.createElement("button");
+    ricButton10.innerHTML = "10€";
+    ricButton10.setAttribute("id", "ric10");
+    ricButton10.addEventListener("click", () => {
+      user.smartphone?.ricarica(10);
+      divConsole.innerHTML = `La ricarica di 10€ è stata effettuata. Nuovo credito: ${user.smartphone?.saldo}€`;
+    });
+    // creo il bottone 15€ metto l'id e creo l'event listener
+    let ricButton15 = document.createElement("button");
+    ricButton15.innerHTML = "15€";
+    ricButton15.setAttribute("id", "ric15");
+    ricButton15.addEventListener("click", () => {
+      user.smartphone?.ricarica(15);
+      divConsole.innerHTML = `La ricarica di 15€ è stata effettuata. Nuovo credito: ${user.smartphone?.saldo}€`;
+    });
+    //   creo il bottone per visualizzare il saldo
+    let btnSaldo = document.createElement("button");
+    btnSaldo.classList.add("btn-big");
+    btnSaldo.innerHTML = "SALDO";
+    btnSaldo.addEventListener("click", () => {
+      user.smartphone?.numero404();
+      divConsole.innerHTML = `Il tuo saldo è: ${user.smartphone?.saldo}€`;
+    });
+    //   creo il bottone per visualizzare il numero di chiamate
+    let btnChiamate = document.createElement("button");
+    btnChiamate.innerHTML = "chiamate";
+    btnChiamate.classList.add("btn-big");
+    btnChiamate.addEventListener("click", () => {
+      divConsole.innerHTML = `Chiamate effettuate: ${user.smartphone?.getNumeroChiamate()}`;
+    });
+    //   creo il bottone per azzerare le chiamate
+    let btnAzzera = document.createElement("button");
+    btnAzzera.innerHTML = "azzera chiamate";
+    btnAzzera.classList.add("btn-big");
+    btnAzzera.addEventListener("click", () => {
+      user.smartphone?.azzeraChiamate();
+      divConsole.innerHTML = `Chiamate effettuate: 0`;
+    });
+    // inserisco tutto nel div infos
+    divInfos.append(h3Name, h2Title, ricButton5, ricButton10, ricButton15, btnSaldo, btnChiamate, btnAzzera);
+    //   inserisco il div infos nel div totale screen
+    phoneScreen.appendChild(divInfos);
+    //   creo il div Console e lo aggiungo al div totale screen
+    let divConsole = document.createElement("div");
+    divConsole.classList.add("phone-console");
+    phoneScreen.appendChild(divConsole);
+    // creo il bottone chiamata e lo aggiungo al div totale screen
+    let callBtn = document.createElement("button");
+    callBtn.setAttribute("id", "btn-phone");
+    callBtn.innerHTML = '<i class="fa-solid fa-phone"></i>';
+    let onCall: boolean = false;
+    callBtn.addEventListener("click", () => {
+      if (!onCall) {
+        startTimer(divConsole);
+        onCall = true;
+        callBtn.style.backgroundColor = "red";
+      } else {
+        onCall = false;
+        callBtn.style.backgroundColor = "";
+        clearInterval(interval);
+        console.log(Number(s));
+        let minuti: number = s;
+        console.log(minuti);
+        user.smartphone?.chiamata(minuti);
+        divConsole.innerHTML = `Chiamata terminata. Costo totale: ${user.smartphone?.chiamata(minuti)}€`;
+      }
+    });
+    phoneScreen.appendChild(callBtn);
+    divContainer.appendChild(phoneScreen);
+    container.appendChild(divContainer);
+  }
 }
 
 onload = () => {
@@ -234,6 +235,7 @@ onload = () => {
 };
 
 let interval: number;
+let s: number;
 function startTimer(elem: HTMLElement) {
   let s = 1;
   interval = setInterval(function () {
@@ -244,7 +246,8 @@ function startTimer(elem: HTMLElement) {
     }
     s++;
   }, 1000);
+  console.log(s);
+  return s;
 }
 
 // TODO: sistemare funzionamento del timer delle chiamate che mo pare drogato
-// TODO: altri due smartphone - (forse un ciclo? array di user 1 - 2 - 3 ecc ????)
